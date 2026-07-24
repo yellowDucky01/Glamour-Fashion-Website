e3d7cec9e97f6cc297a4b501edb41e8e3be46ede
yellowDucky01 <devmanuel093@gmail.com>
2026-07-24 21:58:25 +0800

    fixed somethings

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,18 +1,55 @@
-const images = {
-    "image/showcase-image-1.png",
-    "image/showcase-image-2.png",
-    "image/showcase-image-3.png",
-    "image/showcase-image-4.png",
-};
+const images = [
+    "../image/showcase-image-1.jpg",
+    "../image/showcase-image-2.png",
+    "../image/showcase-image-3.png",
+    "../image/showcase-image-4.png",
+];
 
 let currentIndex = 0;
 
-const imageShowcase = document.getElementById('showcasse-image');
+
+const imageShowcase = document.getElementById('showcase-image');
+imageShowcase.src = images[0];
+
 const menButton = document.getElementById('btn-male');
 const womenButton = document.getElementById('btn-female');
 
-menButton.AddEventListener('click', function() {
+menButton.addEventListener('click', function() {
     currentIndex = (currentIndex + 1) % images.length;
 
     imageShowcase.src = images[currentIndex];
 });
+
+
+womenButton.addEventListener('click', function() {
+    currentIndex = (currentIndex + 1) % images.length;
+
+    imageShowcase.src = images[currentIndex];
+});
+
+console.log("main.js loaded successfully!");
+
+
+
+// Login/Sign-up modal
+const loginButton = getElementById('loginBtn');
+
+window.addEventListener('click', function() {
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
